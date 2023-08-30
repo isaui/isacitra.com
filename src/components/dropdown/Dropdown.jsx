9ad3228db9ac5f4cf0b4f35ba2cbbfb5a6efe33b
isaui <isadestroyed17@gmail.com>
2023-08-30 17:51:55 +0700

    bagus

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -88,6 +88,7 @@ const Dropdown = ({ options, onSelect, posts=[]}) => {
                 <button
                   key={option.value}
                   onClick={() => handleOptionClick(option.value, option.label)}
+                  onPointerDown={() => handleOptionClick(option.value, option.label)}
                   className="block w-full text-left px-4 py-2 text-sm text-gray-300 hover:bg-gray-700 hover:text-white"
                   role="menuitem"
                 >
