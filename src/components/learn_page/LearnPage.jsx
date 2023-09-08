fd8f3025feb339342bb401484692f835579e5ea5
isaui <isadestroyed17@gmail.com>
2023-09-08 23:15:15 +0700

    s

--- src/components/learn_page/LearnPage.jsx
+++ src/components/learn_page/LearnPage.jsx
@@ -14,17 +14,7 @@ export default function ({statusCode, message}) {
             <HomepageNav/>
          <div className=' min-h-screen w-full max-w-[1240px] flex flex-col'>
             <MobileSearchbar/>
-            <div className="flex items-center my-auto h-full">
-                <div className=" my-auto h-full flex justify-center items-center mx-auto md:flex-row flex-col-reverse">
-                <div className="mx-8">
-                <h1 className=" text-red-400 lg:text-5xl md:text-4xl text-3xl mx-auto md:text-left text-center mt-3">{!statusCode? '404' : statusCode}</h1>
-                <p className=" text-gray-200 lg:text-2xl md:text-2xl text-lg text-center md:text-left"> {!message? 'Maaf Halaman Tidak Ditemukan.' : message} <br></br> Silahkan Kembali Hehey.</p>
-                </div>
-                <div className="">
-                    <img className= " lg:min-h-[200px] lg:h-[210px] md:min-h-[180px] md:h-[190px] h-[180px]"src={Error} alt="" />
-                </div>
-                </div>
-            </div>
+            
         </div>
         </div>
         <Footer/>
@@ -49,7 +39,7 @@ const MobileSearchbar = () => {
     setQuery('');
   };
   //fixed top-[64px] left-0
-    return (<div className=" bg-red-600 py-2 px-4  w-full z-10"> 
+    return (<div className=" bg-red-600 py-2 px-4  w-full relative"> 
     <form className="flex items-center" onSubmit={handleSubmit}>
       <input
         type="text"
