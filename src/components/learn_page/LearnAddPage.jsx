205486535c1e3d9f498bf4f5d216f0743cecedd9
isaui <isadestroyed17@gmail.com>
2023-09-09 10:24:39 +0700

    x

--- src/components/learn_page/LearnAddPage.jsx
+++ src/components/learn_page/LearnAddPage.jsx
@@ -21,12 +21,12 @@ export default function () {
             </button>
             </div>
 
-            <div className=" my-2 mx-4 px-8">
+            <div className=" my-2 mx-4 px-8 w-full bg-yellow-400">
             <label htmlFor="title" class="block mb-2 text-sm font-medium text-white">Title</label>
             <input value={title} onChange={e => setTitle(e.target.value)}type="text" id="title" class=" bg-slate-800 focus:border  text-white  text-sm rounded-xs focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Nama Mata Kuliah"></input>
         </div>
 
-        <div className=" my-2 mx-4 px-8">
+        <div className=" my-2 mx-4 px-8 w-full bg-yellow-400">
             <label htmlFor="semester" class="block mb-2 text-sm font-medium text-white">Semester</label>
             <input value={semester} onChange={e => setSemester(e.target.value)}type="text" id="semester" class=" bg-slate-800 focus:border  text-white  text-sm rounded-xs focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Semester 3"></input>
         </div>
