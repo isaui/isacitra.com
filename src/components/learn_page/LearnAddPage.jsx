ac74be3b108b6e17b999ac420aed3307ff43c654
isaui <isadestroyed17@gmail.com>
2023-09-09 10:13:38 +0700

    j

--- src/components/learn_page/LearnAddPage.jsx
+++ src/components/learn_page/LearnAddPage.jsx
@@ -12,7 +12,7 @@ export default function () {
         <div className="">
         <div className=' min-h-screen flex justify-center flex-col items-center w-full'>
             <HomepageNav/>
-         <div className=' min-h-screen w-full max-w-[1240px] flex flex-col'>
+         <div className=' min-h-screen w-full max-w-5xl flex flex-col'>
          <div className="md:mt-20 mt-24 flex justify-between items-center text-white w-full   py-3 px-8 bg-slate-900 ">
             <h1 className=" text-2xl md:text-3xl ">Tambahkan Mata Kuliah</h1>
             <button className=" bg-neutral-950 hover:bg-neutral-800 rounded-sm px-4 py-2 text-base mr-8">
@@ -20,7 +20,7 @@ export default function () {
             </button>
             </div>
 
-            <div className=" my-2 mx-4">
+            <div className=" my-2 mx-4 px-8">
             <label htmlFor="title" class="block mb-2 text-sm font-medium text-white">Title</label>
             <input value={title} onChange={e => setTitle(e.target.value)}type="text" id="title" class=" bg-slate-800 focus:border  text-white  text-sm rounded-xs focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Nama Mata Kuliah"></input>
         </div>
