9932ee030bb7339d2a24e54ef407d90db5e4f685
isaui <isadestroyed17@gmail.com>
2023-09-09 10:09:06 +0700

    hh n bad

--- src/components/learn_page/LearnAddPage.jsx
+++ src/components/learn_page/LearnAddPage.jsx
@@ -7,17 +7,23 @@ import { useNavigate} from 'react-router-dom';
 import ZeroArticle from '../../assets/Zero/zero.svg';
 
 export default function () {
+    const [title, setTitle] = useState('');
     return (
         <div className="">
         <div className=' min-h-screen flex justify-center flex-col items-center w-full'>
             <HomepageNav/>
          <div className=' min-h-screen w-full max-w-[1240px] flex flex-col'>
-         <div className="md:mt-20 mt-24 flex justify-between items-center text-white w-full   py-3 px-8 z-20 bg-slate-900 ">
+         <div className="md:mt-20 mt-24 flex justify-between items-center text-white w-full   py-3 px-8 bg-slate-900 ">
             <h1 className=" text-2xl md:text-3xl ">Tambahkan Mata Kuliah</h1>
             <button className=" bg-neutral-950 hover:bg-neutral-800 rounded-sm px-4 py-2 text-base mr-8">
                 Simpan Matkul
             </button>
             </div>
+
+            <div className=" my-2 mx-4">
+            <label htmlFor="title" class="block mb-2 text-sm font-medium text-white">Title</label>
+            <input value={title} onChange={e => setTitle(e.target.value)}type="text" id="title" class=" bg-slate-800 focus:border  text-white  text-sm rounded-xs focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Nama Mata Kuliah"></input>
+        </div>
         </div>
         </div>
         <Footer/>
