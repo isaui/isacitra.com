ac23f5bf2dbb655d9d84e512b8934201da0bca15
isaui <isadestroyed17@gmail.com>
2023-09-09 17:36:46 +0700

    yeah

--- src/components/learn_page/ContentLearnPage.jsx
+++ src/components/learn_page/ContentLearnPage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { HomepageNav } from "../nav/Nav";
 import Footer from "../footer/Footer";
 import Error from '../../assets/error/error.svg'
+import { FaBook } from "react-icons/fa";
 
 export default function () {
     return (
         <div className="">
         <div className=' min-h-screen flex justify-center flex-col items-center w-full'>
+            <button className="fixed top-0 left-0  p-4 rounded-md bg-neutral-950 hover:bg-neutral-800"><FaBook color="white"/></button>
         </div>
-        <Footer/>
     </div>
     )
 }
