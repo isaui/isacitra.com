fd2fa7da21e910e6ceb5f05910c071fbac914ccf
isaui <isadestroyed17@gmail.com>
2023-09-09 17:51:45 +0700

    r

--- src/components/learn_page/ContentLearnPage.jsx
+++ src/components/learn_page/ContentLearnPage.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { HomepageNav } from "../nav/Nav";
 import Footer from "../footer/Footer";
 import Error from '../../assets/error/error.svg'
 import { FaBook } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function () {
+    const [sidebarActive, setSidebarActive] = useState(false);
+    const toggleSidebar = () => {
+        setSidebarActive(prev => !prev);
+    }
     return (
         <div className="">
         <div className=' min-h-screen flex justify-center flex-col items-center w-full'>
-            <button className="fixed top-3 left-5  p-4 rounded-md bg-neutral-950 hover:bg-neutral-800"><FaBook  size={24} color="white"/></button>
+            <button onClick={()=> {
+                toggleSidebar()
+            }}className="fixed top-3 left-5  p-3 rounded-lg bg-neutral-950 hover:bg-neutral-800">{
+            sidebarActive? <AiOutlineClose size={24} color="white"/> : <FaBook  size={24} color="white"/>
+            }</button>
         </div>
     </div>
     )
