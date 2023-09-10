fb5a7a213afdf2a57730fc6f2756129a1d9b9b01
isaui <isadestroyed17@gmail.com>
2023-09-10 20:58:12 +0700

    hello

--- src/components/learn_page/LearnAddPage.jsx
+++ src/components/learn_page/LearnAddPage.jsx
@@ -14,6 +14,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useSelector } from "react-redux";
 import axios from "axios";
+import mongoose from "mongoose";
 
 
 
@@ -85,6 +86,7 @@ export default function () {
       
       try {
           const data = {
+            _id: new mongoose.Types.ObjectId(),
             title:title,
             semester:semester,
             categories:categories,
