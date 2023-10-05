50476fc35f63dda040b2f551539b920c5646e272
isaui <isadestroyed17@gmail.com>
2023-10-05 22:24:04 +0700

    cuk

--- src/components/video_conference/Join.jsx
+++ src/components/video_conference/Join.jsx
@@ -523,12 +523,12 @@ const userVideoSetting = {
 const Sidebar = ({isOpen, closeSidebar})=>{
   
   return <div onClick={(e)=>{e.stopPropagation()}} className={`fixed  bottom-0 left-0 flex flex-col min-w-[18rem] h-screen max-h-screen w-screen md:max-w-[40%] lg:max-w-[30%] bg-slate-800 `}>
-    <div className="bg-slate-950 px-2 py-2 flex items-center justify-between w-full">
-      <h1 className=" text-white text-2xl">CHATS</h1>
-      <AiFillCloseCircle onClick={closeSidebar} color="#00A8FF" className=" w-8 h-8 "/>
+    <div className="bg-slate-950 h-16 flex items-center justify-between w-full">
+      <h1 className="ml-2 text-white text-2xl">CHATS</h1>
+      <AiFillCloseCircle onClick={closeSidebar} color="#00A8FF" className="mr-2 w-8 h-8 "/>
     </div>
-    <div className="my-auto bg-slate-800">
-      <h1 className="mx-auto my-auto text-white text-sm">Belum ada pesan</h1>
+    <div className="my-auto w-full bg-slate-800">
+      <h1 className=" my-aut mx-auto text-white text-sm">Belum ada pesan</h1>
     </div>
     <div className=" my-2 mx-2 min-h-[3rem] flex justify-between items-center bg-slate-950">
       <input type="text" className="text-white grow text-sm py-3 px-2 rounded-md outline-none bg-transparent focus:border-[#00A8FF] border-2 border-[#00A8FF]" placeholder="Masukkan pesan Anda"/>
